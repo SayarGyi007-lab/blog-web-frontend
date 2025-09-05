c43dd1d6ed912a9bb42166e3d8a0cbda3bd56d7d
SayarGyi007-lab <thss152021@gmail.com>
2025-09-06 02:21:27 +0700

    first draft

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -48,9 +48,9 @@ function Login() {
   return (
     <div className="relative flex items-center justify-center min-h-screen bg-gradient-to-br from-indigo-200 via-white to-pink-200 overflow-hidden">
      
-      {/* <div className="absolute top-[-8rem] left-[-8rem] w-96 h-96 bg-indigo-300 rounded-full mix-blend-multiply filter blur-3xl opacity-40 animate-blob"></div>
+      <div className="absolute top-[-8rem] left-[-8rem] w-96 h-96 bg-indigo-300 rounded-full mix-blend-multiply filter blur-3xl opacity-40 animate-blob"></div>
       <div className="absolute bottom-[-8rem] right-[-8rem] w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-3xl opacity-40 animate-blob animation-delay-2000"></div>
-      <div className="absolute top-1/2 left-1/2 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-3xl opacity-40 animate-blob animation-delay-4000"></div> */}
+      <div className="absolute top-1/2 left-1/2 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-3xl opacity-40 animate-blob animation-delay-4000"></div>
 
       
       <div className="w-full max-w-md bg-white/70 backdrop-blur-xl rounded-2xl shadow-2xl p-8 border border-white/30 relative z-10">
--- src/slices/api.ts
+++ src/slices/api.ts
@@ -15,14 +15,3 @@ export const apiSlice = createApi({
     endpoints: () => ({})
 })
 
-// export const apiSlice = createApi({
-//     reducerPath: 'apiSlice',
-//     baseQuery: fetchBaseQuery({
-//         baseUrl: `${baseUrl}`,
-//         credentials: "include",
-        
-//     }),
-//     endpoints: () => ({}),
-    
-// })
-
